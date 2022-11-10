84f432fab9c818a94ec438b21fcc67974ddc7e74
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-11-10 10:49:39 +0000

    Add Project tests

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -7,16 +7,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Tree<int> tree = new Tree<int>(true);
+        Console.WriteLine("InOrder");
+        Tree<int> tree = new Tree<int>();
 
         tree.Add(1);
         tree.Add(3);
         tree.Add(2);
 
         tree.Add(5);
-        tree.Add(0);
 
         tree.Traverse();
+
+        Console.WriteLine("PreOrder");
+        Tree<int> tree2 = new Tree<int>(true);
+
+        tree2.Add(1);
+        tree2.Add(3);
+        tree2.Add(2);
+
+        tree2.Add(5);
+
+        tree2.Traverse();
+
         //GenericEnumerableList<string> list = new GenericEnumerableList<string>(new string[] {"1","2","3" });
         //foreach (string item in list)
         //{
